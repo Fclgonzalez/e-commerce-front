42ada35cd2b9fc1d4bddc6746aa3788e88b81c7b
Lucas-Lopes-Portella <lucaslopesportella18@outlook.com>
2022-08-03 14:16:48 -0300

    Configurações iniciais do angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD6yYz889SWt4DJafdM2bsCypVLqUhm0oQ',
+    authDomain: 'ecommerce-1d0a1.firebaseapp.com',
+    projectId: 'ecommerce-1d0a1',
+    storageBucket: 'ecommerce-1d0a1.appspot.com',
+    messagingSenderId: '511871010828',
+    appId: '1:511871010828:web:b38a67c1142d7632f94629',
+    measurementId: 'G-272DVE4P4G',
+  },
 };
 
 /*
