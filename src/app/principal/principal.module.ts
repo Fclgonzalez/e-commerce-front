b7f3e8813422bcee1a685df0d04b7fd922e5e2ba
MarcosASampaio <marcosgamer@hotmail.com>
2022-08-09 11:30:00 -0300

    Preparação da estrutura inicial das rotas

--- src/app/principal/principal.module.ts
+++ src/app/principal/principal.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PrincipalRoutingModule } from './principal-routing.module';
+import { ComponentesCompartilhadosModule } from '../componentes-compartilhados/componentes-compartilhados.module';
+import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PaginaInicialComponent,
+    PrincipalComponent
+  ],
   imports: [
     CommonModule,
-    PrincipalRoutingModule
+    PrincipalRoutingModule,
+    ComponentesCompartilhadosModule
   ]
 })
 export class PrincipalModule { }
