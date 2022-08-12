f329619a00a2280c84b2b0df936106d55fb41b95
MarcosASampaio <marcosgamer@hotmail.com>
2022-08-12 09:04:09 -0300

    Estrutura da página inicial

--- src/app/principal/principal.module.ts
+++ src/app/principal/principal.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PrincipalRoutingModule } from './principal-routing.module';
 import { ComponentesCompartilhadosModule } from '../componentes-compartilhados/componentes-compartilhados.module';
 import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
+import { MaterialModule } from '../material/material.module';
+import { TermosDeUsoComponent } from './pages/termos-de-uso/termos-de-uso.component';
+import { SaibaMaisComponent } from './pages/saiba-mais/saiba-mais.component';
+import { PerguntasFrequentesComponent } from './pages/perguntas-frequentes/perguntas-frequentes.component';
 
 
 @NgModule({
@@ -20,7 +23,6 @@ import { PrincipalComponent } from './pages/principal/principal.component';
     MaterialModule,
     PrincipalRoutingModule,
     ComponentesCompartilhadosModule,
-    MaterialModule
   ]
 })
 export class PrincipalModule { }
