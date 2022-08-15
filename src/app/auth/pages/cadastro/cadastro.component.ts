fcb7e38ff4270c9d9e3610534e1a0144e99e3794
Fclgonzalez <100890434+Fclgonzalez@users.noreply.github.com>
2022-08-15 09:31:27 -0300

    add métodos das páginas de login e cadastro

--- src/app/auth/pages/cadastro/cadastro.component.ts
+++ src/app/auth/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { User } from 'src/app/user/models/user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  cadastroForm: FormGroup= this.fb.group({
+    nome: ['', [Validators.required]],
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    identificacao: ['', [Validators.required]]
+  })
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private snackbar: MatSnackBar,
+    private router: Router,
+    private title: Title
+  ) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Cadastre-se')
+  }
+
+  cadastrarConsumidor(): void {
+    const login: User = this.cadastroForm.value
+    this.authService.cadastrarConsumidor(login)
+      .subscribe(
+        () => {
+          this.snackbar.open('Cadastrado com sucesso.', 'Ok', {
+            duration: 3000       
+        })
+        /* this.router.navigateByUrl('/auth/confirmar-email') */
+  })
   }
 
 }
