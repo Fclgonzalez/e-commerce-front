0b90729e0edfef2baec5ad7b4d1bb8f12ac5fcc2
Fclgonzalez <100890434+Fclgonzalez@users.noreply.github.com>
2022-08-15 13:37:25 -0300

    user service e token interceptor

---
+++ src/app/user/services/user.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly baseUrl: string = 'http://localhost:8080/imobil/usuarios'
+
+  constructor(
+    private http: HttpClient
+      ) { }
+
+  getUsuarios(): Observable<User[]> {
+    return this.http.get<User[]>(this.baseUrl)
+  }
+
+  getUsuarioById(idUser: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/${idUser}`)
+  }
+  
+  getUsuarioByRoleId(idRole: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}/role/${idRole}`)
+  }
+
+  patchUsuario(username: string, user: User): Observable<User> {
+    return this.http.patch<User>(`${this.baseUrl}/${username}`, user)
+  }
+  
+  patchDesabilitarUsuario(username: string, user: User): Observable<User> {
+    return this.http.patch<User>(`${this.baseUrl}/${username}/disable`, user)
+  }
+  
+  patchAdminDesabilitarUsuario(username: string, user: User): Observable<User> {
+    return this.http.patch<User>(`${this.baseUrl}/${username}/admin`, user)
+  }
+  
+  deleteUsuario(idUser: number): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/${idUser}`)
+  }
+
+}
