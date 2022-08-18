10e4bec1cdad0203af4351c98e34b71fee7a6a49
nataliaviana <naati.viana@hotmail.com>
2022-08-18 12:59:38 -0300

    adicionado pagina imovel

--- src/app/imoveis/pages/imovel/imovel.component.ts
+++ src/app/imoveis/pages/imovel/imovel.component.ts
@@ -1,6 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Enderecos } from 'src/app/enderecos/models/endereco';
 import { Imovel } from '../../models/imovel';
 import { ImoveisService } from '../../service/imoveis.service';
 
@@ -11,76 +12,31 @@ import { ImoveisService } from '../../service/imoveis.service';
 })
 export class ImovelComponent implements OnInit {
 
-  formImovel: FormGroup =  this.fb.group({
-    idImovel: ['', [ Validators.required ]],
-    titulo: ['', [ Validators.required ]],
-    dataCriacao: ['', [ Validators.required ]],
-    contratoAluguel: ['', [ Validators.required ]],
-    contratoVenda: ['', [ Validators.required ]],
-    valorAluguel: ['', [ Validators.required ]],
-    valorVenda: ['', [ Validators.required ]],
-    area: ['', [ Validators.required ]],
-    descricao: [''],
-    quartos: ['', [ Validators.required ]],
-    suite: [''],
-    banheiros: ['', [ Validators.required ]],
-    vagas: ['', [ Validators.required ]],
-    finalidadeImovel: ['', [ Validators.required ]],
-    tipoImovel: ['', [ Validators.required ]],
-    caracteristicas: [''],
-    endereco: [''],
-    user: ['', [ Validators.required ]]
-  })
+  imovel?: Imovel
+  endereco?: Enderecos
 
-  imovel!: Imovel
+  naoEncontrado: boolean = false
 
   constructor(
     private imovelService: ImoveisService,
-    private fb: FormBuilder,
     private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
       (params) => {
-        let idImovel = parseInt(params.get('idImovel') ?? '0')
-        this.recuperarImovel(idImovel)
+        let id = parseInt(params.get('idImovel') ?? '0')
+        this.buscaImovel(id)
       }
     )
   }
 
-  recuperarImovel(id: number): void {
-    this.imovelService.getImoveisById(id)
-    .subscribe(
-      imo => {
-        this.imovel = imo
-        this.formImovel.setValue({
-          idImovel: this.imovel.idImovel,
-          titulo: this.imovel.titulo,
-          dataCriacao: this.imovel.dataCriacao,
-          contratoAluguel: this.imovel.contratoAluguel,
-          contratoVenda: this.imovel.contratoVenda,
-          valorAluguel: this.imovel.valorAluguel,
-          valorVenda: this.imovel.valorVenda,
-          area: this.imovel.area,
-          descricao: this.imovel.descricao,
-          quartos: this.imovel.quartos,
-          suite: this.imovel.suite,
-          banheiros: this.imovel.banheiros,
-          vagas: this.imovel.vagas,
-          finalidadeImovel: this.imovel.finalidadeImovel,
-          tipoImovel: this.imovel.tipoImovel,
-          caracteristicas: this.imovel.caracteristicas,
-          endereco: this.imovel.endereco,
-          user: this.imovel.user
-        })
-      }
-      // (erro: HttpErrorResponse) => {
-      //   this.naoEncontrado = erro.status == 404
-      // }
-    )
+  buscaImovel(id: number) {
+    this.imovelService.getImoveisById(id).subscribe(async (dadosImovel) => {
+      this.imovel = dadosImovel
+    },
+    (erro: HttpErrorResponse) => {
+      this.naoEncontrado = erro.status == 404
+    })
   }
 }
-
-
-
