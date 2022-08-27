e18e467d0049cfb5b4286d6be9b3fd04372a295d
Lucas Oliveira <lucas.oliveirab0531@gmail.com>
2022-08-26 21:26:38 -0300

    Configurações para deploy da app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD6yYz889SWt4DJafdM2bsCypVLqUhm0oQ',
+    authDomain: 'ecommerce-1d0a1.firebaseapp.com',
+    projectId: 'ecommerce-1d0a1',
+    storageBucket: 'ecommerce-1d0a1.appspot.com',
+    messagingSenderId: '511871010828',
+    appId: '1:511871010828:web:b38a67c1142d7632f94629',
+    measurementId: 'G-272DVE4P4G',
+  },
 };
