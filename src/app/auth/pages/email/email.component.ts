abd0f4f92ae3b82edf56b5f371cd55fd2217069c
nataliaviana <naati.viana@hotmail.com>
2022-08-27 17:41:52 -0300

    adicionado title nas guias

--- src/app/auth/pages/email/email.component.ts
+++ src/app/auth/pages/email/email.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-email',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private title: Title
+  ) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Email de Confirmação')
   }
 
 }
